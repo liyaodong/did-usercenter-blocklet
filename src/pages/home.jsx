0da391a5ea52578c4e797cf0c9151d957f04704a
Yaodong Li <mail@liyaodong.com>
2024-03-29 17:25:21 +0800

    Init tailwindcss

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,14 +4,12 @@ import logo from '../logo.svg';
 
 function Home() {
   return (
-    <header className="app-header">
-      <img src={logo} className="app-logo" alt="logo" />
+    <header>
+      <img src={logo} className="h-6 w-6" alt="logo" />
       <pre style={{ textAlign: 'left' }}>
         <code>window.blocklet = {JSON.stringify(window.blocklet, null, 2)}</code>
       </pre>
-      <Link className="app-link" to="/about">
-        About
-      </Link>
+      <Link to="/about">About</Link>
       <a className="app-link" href="https://developer.blocklet.io/docs/" target="_blank" rel="noopener noreferrer">
         Learn Blocklet
       </a>
